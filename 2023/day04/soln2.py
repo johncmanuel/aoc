4d336c1543294117172a4411cc71cb4ec19df929
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2023-12-15 22:14:09 -0800

    updated

--- 2023/day04/soln2.py
+++ 2023/day04/soln2.py
@@ -11,6 +11,7 @@ class Solution:
     def solve(self) -> None:
         count = 0
         copies: dict[str, list[str]] = {}
+
         for c, cc in enumerate(self.lines):
             k = c+1
             if k not in copies: copies[k] = [cc]
@@ -21,14 +22,14 @@ class Solution:
             win_nums = list(filter(len, c[0].split(":")[1].split(" ")))
             my_nums = list(filter(len, c[1].split(" ")))
             matching_nums = 0
+            
             for n in my_nums:
                 if n in win_nums: matching_nums += 1
                     
             if matching_nums > 0:
                 counter = 0
-                num_copies_curr_card = len(copies[card_num])
                 # want to copy cards based on num of copies off current card
-                while counter < num_copies_curr_card:
+                while counter < len(copies[card_num]):
                     # copy cards starting from current card to the card based on # of matching nums
                     for i in range(card_num, card_num+matching_nums):
                         copies[i+1].append(self.lines[i])
@@ -36,6 +37,7 @@ class Solution:
         
         for k, v in copies.items():
             count += len(v)
+            
         print(count)
 
 s = Solution()
