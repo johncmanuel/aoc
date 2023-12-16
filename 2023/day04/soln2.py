506ccc0689a27507a01fc1684b2e83c10cf8815f
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2023-12-15 21:59:03 -0800

    day 4 pt 2 soln

---
+++ 2023/day04/soln2.py
@@ -0,0 +1,42 @@
+from lib.aoc import extract_input
+import os
+
+class Solution:
+
+    def __init__(self) -> None:
+        # get path of input file
+        dir_path = os.path.dirname(os.path.realpath(__file__)).split("\\")
+        self.lines = extract_input(f"./{dir_path[-2]}/{dir_path[-1]}/input.txt")
+
+    def solve(self) -> None:
+        count = 0
+        copies: dict[str, list[str]] = {}
+        for c, cc in enumerate(self.lines):
+            k = c+1
+            if k not in copies: copies[k] = [cc]
+            else: copies[k].append(cc)
+
+        for card_num, card in enumerate(self.lines, 1):
+            c = card.split("|")
+            win_nums = list(filter(len, c[0].split(":")[1].split(" ")))
+            my_nums = list(filter(len, c[1].split(" ")))
+            matching_nums = 0
+            for n in my_nums:
+                if n in win_nums: matching_nums += 1
+                    
+            if matching_nums > 0:
+                counter = 0
+                num_copies_curr_card = len(copies[card_num])
+                # want to copy cards based on num of copies off current card
+                while counter < num_copies_curr_card:
+                    # copy cards starting from current card to the card based on # of matching nums
+                    for i in range(card_num, card_num+matching_nums):
+                        copies[i+1].append(self.lines[i])
+                    counter += 1
+        
+        for k, v in copies.items():
+            count += len(v)
+        print(count)
+
+s = Solution()
+s.solve()
