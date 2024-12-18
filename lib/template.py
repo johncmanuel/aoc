7e630dc8cd3f5c4f11c7e264d1841e95f751c330
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-12-18 10:06:22 -0800

    update template

--- lib/template.py
+++ lib/template.py
@@ -7,9 +7,10 @@ class Solution:
     def __init__(self) -> None:
         # sef.lines contains the input data from AOC
         dir_path = os.path.dirname(os.path.realpath(__file__)).split("\\")
-        input_path = f"{dir_path[-1]}/input.txt"
+        input_name = "input"
+        input_path = f"{dir_path[-1]}/{input_name}.txt"
         if len(dir_path) > 1:
-            input_path = f"{dir_path[-2]}/{dir_path[-1]}/input.txt"
+            input_path = f"{dir_path[-2]}/{dir_path[-1]}/{input_name}.txt"
         self.lines = extract_input(input_path)
 
     def solve_part_1(self) -> None:
